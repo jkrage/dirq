39208c9ac78393ac9f8744ff63c89c6c4f3b7fd5
Joshua Krage <jkrage@guisarme.us>
2016-12-21 17:09:24 -0500

    Improve docstrings

--- DirectoryQuery/utils.py
+++ DirectoryQuery/utils.py
@@ -1,45 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-    utils.py
+"""utils.py
 
-    DirectoryQuery, utility functions
+   DirectoryQuery utility functions
 
 """
 import string
 
 
 def get_format_fields(format_string):
-    """ :type format_string: string.Formatter format_string
+    """Parse a string.format() format_string specification and extract
+       any named fields that will be needed by the output.
 
-        Parse a string.format() format_string specification and extract
-        any named fields that will be needed by the output.
+       Ignore position-based (name is all numeric) and
+       order-based/un-named fields (e.g., {}).
 
-        Ignore position-based (name is all numeric) and
-        order-based/un-named fields (e.g., {}).
+       This can be used to ensure those fields are available to the formatter
+       prior to needing them.
 
-        This can be used to ensure those fields are available to the formatter
-        prior to needing them.
-
-        :rtype: set() of named fields
+       :param format_string: string.Formatter String supplied to format()
+       :type format_string: string
+       :return: set of format fields to be interpolated by format()
+       :rtype: set
     """
     assert format_string is not None
     return set([f[1] for f in string.Formatter().parse(format_string)])
 
 
 def get_named_format_fields(format_string):
-    """ :type format_string: string.Formatter format_string
+    """Parse a string.format() format_string specification and extract
+       any named fields that will be needed by the output.
 
-        Parse a string.format() format_string specification and extract
-        any named fields that will be needed by the output.
+       Ignore position-based (name is all numeric) and
+       order-based/un-named fields (e.g., {}).
 
-        Ignore position-based (name is all numeric) and
-        order-based/un-named fields (e.g., {}).
+       This can be used to ensure those fields are available to the formatter
+       prior to needing them.
 
-        This can be used to ensure those fields are available to the formatter
-        prior to needing them.
-
-        :rtype: set() of named fields
+       :param format_string: string.Formatter String supplied to format()
+       :type format_string: string
+       :return: set of format fields to be interpolated by format()
+       :rtype: set
     """
     assert format_string is not None
     return set([f for f in get_format_fields(format_string) if f and not f.isnumeric()])
